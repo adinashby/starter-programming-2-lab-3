2282ebeb50a6fb3793782c380eb7dbce4f5b813f
Adin Ashby <47254289+adinashby@users.noreply.github.com>
2023-02-06 12:11:54 -0500

    Update LabThree.java

--- src/main/java/com/prog2/labs/LabThree.java
+++ src/main/java/com/prog2/labs/LabThree.java
@@ -14,51 +14,15 @@ public class LabThree {
 	}
 	
 	/**
-	 * 1) Write a method that will create an array of an specified size. Then, start
-	 * filling the array using startingInteger in the last position and incremented 
-	 * by 3 for each previous position. Once this is done rotate all the entries 
-	 * with even indexes 2 positions to the left (if the size is 5, whatever is in 
-	 * index location 4 is stored in index position 2, whatever is in 2, is stored 
-	 * in index position 0, and so on). The first entry is to be stored in the last 
-	 * location (size -1) of the array. Display the resulting array (for your testing 
-	 * purposes only). Return the array after rotation.
-	 * 
-	 * Note 1: You are to expect a perfect user who will always enter an odd array size; 
-	 * that is, do not verify the validity of user input. 
-	 * 
-	 * Note 2: The use of libraries is prohibited. Your program must work for any odd array 
-	 * size and starting value, not just the ones in the samples above. 
-	 * 
-	 * Note 3: You are free to use whichever type of loop you want.
-	 * 
-	 * 
-	 * @param arraySize size of the one-dimensional array (which you can assume is odd and at least 5).
-	 * @param startingInteger starting value to fill the array with.
-	 *
-	 * @return result array after rotation.
+	 * Please refer to the README file for question(s) description
 	 */
 	
 	public int[] rotateTheArray(int arraySize, int startingInteger) {
-		
-		
 		return new int[0];
 	}
 	
-	/**
-	 * 2) Write a Java method that given an array of non-negative integers nums, 
-	 * you are initially positioned at the first index of the array. Each element 
-	 * in the array represents your maximum jump length at that position. Your goal 
-	 * is to reach the last index in the minimum number of jumps. You can assume 
-	 * that you can always reach the last index.
-	 * 
-	 * @param nums input array.
-	 *
-	 * @return result number of jumps needed to reach the last index.
-	 */
 	
 	public int jumpGame(int[] nums) {
-		
-		
 		return 0;
 	}
 }
